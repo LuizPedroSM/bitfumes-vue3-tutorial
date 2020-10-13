49602cbf8dd058542a867c037b1b1a6436da2251
Luiz Pedro <luizpsmoreira@gmail.com>
2020-10-13 19:20:24 -0300

    Markdown

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,25 @@
 <template>
-  <nav class="w-full bg-gradient-to-r from-blue-800 to-blue-600 px-4 py-2">
-    <router-link class="text-white mx-2" to="/dc-heros"> DC Heros</router-link>
-    <router-link class="text-white mx-2" to="/calendar"> Calendar</router-link>
+  <nav
+    class="w-full bg-gradient-to-r from-blue-800 to-blue-600 px-4 py-2 text-white"
+  >
+    <router-link v-for="item in list" :key="item.to" class="mx-2" :to="item.to">
+      {{ item.title }}
+    </router-link>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      list: [
+        { title: "DC Heros", to: "/dc-heros" },
+        { title: "Calendar", to: "/calendar" },
+        { title: "Markdown", to: "/markdown" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
